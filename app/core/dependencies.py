e75f3495448a5d04dc4167371cef27e85c667eb7
Kauser-Ibrahim <kauser4960@gmail.com>
2025-09-01 20:35:15 +0530

    added auths

---
+++ app/core/dependencies.py
@@ -0,0 +1,15 @@
+from fastapi import Header, HTTPException, Security
+from app.core.security import verify_token, get_api_key
+from app.core.config import settings
+
+
+def get_api_key(api_key: str = Header(...)):
+    if api_key != settings.API_KEY:
+        raise HTTPException(status_code=403, detail="Invalid API key")
+
+
+def current_user(token: str = Header(...)):
+    payload = verify_token(token)
+    if not payload:
+        raise HTTPException(status_code=401, detail="Invalid JWT-Token")
+    return payload
